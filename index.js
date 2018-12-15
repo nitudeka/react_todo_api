b657bf9172e6a442fda49c6232d2e37c2e0ecbd6
nitudeka <nitumonideka327@gmail.com>
2018-12-15 19:27:53 +0530

    fixed warning in mongoose

--- index.js
+++ index.js
@@ -6,15 +6,21 @@
 // Dependencies
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const config = require('./src/lib/config');
+// User schema
+const User = require('./src/controllers/models/user');
 // Routes
-const register = require('./routes/register');
+const register = require('./src/controllers/routes/register')
 
 const app = express();
 app.use(bodyParser.json());
 
+// Connect to mongoose database
+mongoose.connect(config.dbUrl, { useNewUrlParser: true, useCreateIndex: true });
 // Required fields:- name, email, password, timestamp
-app.post('/register', (req, res) => register(req, res));
+app.post('/register', (req, res) => register(req, res, User, bcrypt));
 
 app.listen(config.PORT, () => {
   console.log('Server is listening on port', config.PORT);
