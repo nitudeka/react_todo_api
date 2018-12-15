75baf89dfc8ec2f22bee6b1cacd70d6f23babe2e
nitudeka <nitumonideka327@gmail.com>
2018-12-15 11:55:45 +0530

    added user schema

---
+++ src/controllers/models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// User schema defination
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  joined: {
+    type: Date,
+    required: true
+  },
+  tasks: [
+    {
+      date: Number,
+      task: String,
+      status: String
+    }
+  ]
+})
+
+module.exports = mongoose.model('users', userSchema);
