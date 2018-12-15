c5588c7472cfb80846e43b6b2ab82e55311c7e35
nitudeka <nitumonideka327@gmail.com>
2018-12-15 19:34:53 +0530

    added user creating ability

---
+++ src/controllers/routes/register.js
@@ -0,0 +1,35 @@
+module.exports = (req, res, userSchema, bcrypt) => {
+  // Validate the required fields
+  const name = typeof(req.body.name) === 'string' ? req.body.name : false;
+  const email = typeof(req.body.email) === 'string' ? req.body.email : false;
+  const password = typeof(req.body.password) === 'string' ? req.body.password : false;
+  const timestamp = typeof(req.body.timestamp) === 'number' ? req.body.timestamp : false;
+
+  if (name && email && password && timestamp) {
+    // Check if the user already exists
+    userSchema.findOne({ email }, (err, data) => {
+      if (!data) {
+        // Hash the password using bcrypt
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(password, salt, (err, hashedPassword) => {
+            if (!err && hashedPassword) {
+              const userData = { name, email, hashedPassword, joined: timestamp, tasks: [] };
+              const newUser = new userSchema(userData);
+              newUser.save((err, user) => {
+                if (!err && user) {
+                  
+                }
+              })
+            } else {
+              res.status(500).json('Could not hash the password');
+            }
+          })
+        })
+      } else {
+        res.status(400).json('User already exists');
+      }
+    })
+  } else {
+    res.status(400).json('Missing required field(s)');
+  }
+}
--- src/lib/config.js
+++ src/lib/config.js
@@ -7,11 +7,13 @@
 const environments = {};
 
 environments.development = {
-  PORT: 3000
+  PORT: 3000,
+  dbUrl: 'mongodb://localhost/todo'
 };
 
 environments.production = {
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  dbUrl: process.env.dbUrl
 };
 
 const defaultENV = typeof(environments[process.env.NODE_ENV]) !== 'undefined' ? process.env.NODE_ENV : 'development';
