297cd92e48db067c0099f54c12d8cd41874e0423
nitudeka <nitumonideka327@gmail.com>
2018-12-27 18:36:21 +0530

    added JWT secret to config

--- src/lib/config.js
+++ src/lib/config.js
@@ -8,12 +8,14 @@ const environments = {};
 
 environments.development = {
   PORT: 3000,
-  dbUrl: 'mongodb://localhost/todo'
+  dbUrl: 'mongodb://localhost/todo',
+  jwtSecret: 'verySecret'
 };
 
 environments.production = {
   PORT: process.env.PORT,
-  dbUrl: process.env.dbUrl
+  dbUrl: process.env.dbUrl,
+  jwtSecret: process.env.jwtSecret
 };
 
 const defaultENV = typeof(environments[process.env.NODE_ENV]) !== 'undefined' ? process.env.NODE_ENV : 'development';
