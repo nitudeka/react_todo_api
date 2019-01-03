d8d7a36ba35c0247dc0246c1496a69167f989d81
nitudeka <nitumonideka327@gmail.com>
2019-01-03 19:33:08 +0530

    created a hepler function to check if a token is valid

---
+++ src/lib/helpers.js
@@ -0,0 +1,44 @@
+/*
+ * Helper functions for the API
+ *
+ */
+
+// Dependencies
+const jwt = require('jsonwebtoken');
+const config = require('./config');
+const bcrypt = require('bcryptjs');
+const userSchema = require('../controllers/models/user');
+
+// Instentiate the container
+const lib = {};
+
+// Check if a token is valid
+lib.verifyToken = (token) => {
+  // Extract the data from the token
+  let tokenData = false;
+  try {
+    tokenData = jwt.verify(token, config.jwtSecret);
+  } catch (e) {
+    return tokenData;
+  };
+  // Return false if the token does not contain an email and a password field
+  if (tokenData.email && tokenData.password) {
+    userSchema.findOne({ email: tokenData.email }, (err, data) => {
+      if (!err && data) {
+        const validPassword = bcrypt.compareSync(tokenData.password, data.hashedPassword);
+        if (validPassword) {
+          return true;
+        } else {
+          return false;
+        };
+      } else {
+        return false;
+      };
+    });
+  } else {
+    return false;
+  }
+};
+
+// Export the module
+module.exports = lib;
